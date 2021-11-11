132f575a82542d2af6d5f0ed8069fa95cba00927
frnemm004 <emmanuel10francis@gmail.com>
2021-11-11 04:11:35 +0200

    SfMLearner-Pytorch_MVSEC

---
+++ EV-FlowNet/src/example.py
@@ -0,0 +1,16 @@
+#import tensorflow as tf
+
+#hello = tf.constant('Hello, TensorFlow!')
+#sess = tf.Session()
+#sess.run(hello)
+#'Hello, TensorFlow!'
+#a = tf.constant(10)
+#b = tf.constant(32)
+#sess.run(a + b)
+#42
+#sess.close()
+
+import tensorflow as tf
+hello = tf.constant('Hello, TensorFlow!')
+sess = tf.Session()
+print(sess.run(hello))
